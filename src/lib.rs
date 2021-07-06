5b222d06a25710bd92a0ae0aa05da015a6c60158
David Raznick <kindly@gmail.com>
2021-07-06 13:27:08 +0100

    add pyo3 binding

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,94 @@ use std::io::prelude::*;
 use std::io::{ErrorKind, Error as IoError};
 use std::error::Error;
 
+use std::sync::mpsc::{channel};
+use std::thread;
+
+use pyo3::prelude::*;
+use pyo3::types::PyIterator;
+
+
+#[pymodule]
+fn flaterer_rs(_py: Python, m: &PyModule) -> PyResult<()> {
+
+    #[pyfn(m)]
+    fn iterator_flatten(py: Python, mut objs: &PyIterator, output_dir: String, main_table_name: String, force: bool) -> PyResult<()> {
+
+        let flat_files_res = FlatFiles::new (
+            output_dir.to_string(),
+            force,
+            main_table_name,
+            vec![],
+        );
+
+        if flat_files_res.is_err() {
+            let err = flat_files_res.unwrap_err();
+            return Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("{:?}", err)))
+        }
+
+        let mut flat_files = flat_files_res.unwrap(); //already checked error
+
+        let (sender, receiver) = channel();
+
+        let handler = thread::spawn(move || -> PyResult<()> {
+            for value in receiver {
+                if let Err(err) = flat_files.process_value(value) {
+                    return Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("{:?}", err)))
+                }
+            }
+
+            if let Err(err) = flat_files.write_files() {
+                return Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("{:?}", err)))
+            }
+            Ok(())
+        });
+
+        let mut gilpool;
+
+        loop {
+            unsafe {gilpool = py.new_pool();}
+
+            let obj = objs.next();
+            if obj.is_none() { break }
+
+            let result = obj.unwrap(); //checked for none
+
+            let json_bytes = PyAny::extract::<&[u8]>(result?)?;
+
+            match serde_json::from_slice::<Value>(&json_bytes) {
+                Ok(value) => {
+                    if let Err(err) = sender.send(value) {
+                        return Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("{:?}", err)))
+                    }
+                }
+                Err(err) => {
+                    return Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("{:?}", err)))
+                }
+            }
+
+            drop(gilpool)
+        }
+
+        drop(sender);
+
+        match handler.join() {
+            Ok(result) => {
+                if let Err(err) = result {
+                    return Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("{:?}", err)))
+                }
+            }
+            Err(err) => {
+                return Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("{:?}", err)))
+            }
+        }
+        Ok(())
+    }
+
+    Ok(())
+}
+
+
+
 
 #[derive(Clone, Debug)]
 pub enum PathItem {
@@ -27,6 +115,7 @@ impl fmt::Display for PathItem {
 }
 
 
+#[derive(Debug)]
 pub struct FlatFiles {
     output_path: PathBuf,
     main_table_name: String,
@@ -37,7 +126,7 @@ pub struct FlatFiles {
     table_metadata: HashMap<String, TableMetadata>,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 pub struct TableMetadata {
     output_fields: HashMap<String, HashMap<String, String>>,
     fields: Vec<String>,
@@ -364,4 +453,5 @@ fn value_convert(value: Value) -> String {
         }
     }
 }
+
 
